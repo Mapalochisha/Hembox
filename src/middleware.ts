99561c9c2e312874cada09ca28fe3186a303fb51
Mapalochisha <chishamapalo05@gmail.com>
2026-03-11 23:58:44 +0000

    Fix middleware redirect loop

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
@@ -14,10 +15,13 @@ export default withAuth(
     callbacks: {
       authorized({ req, token }) {
         const { pathname } = req.nextUrl;
-        if (pathname === "/admin/login") return true;
+        if (pathname.startsWith("/admin/login")) return true;
         if (pathname.startsWith("/admin")) return !!token;
         return true;
       },
+    },
+    pages: {
+      signIn: "/admin/login",
     },
   }
 );
